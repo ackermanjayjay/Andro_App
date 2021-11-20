a8e686733f254599deac0f5db363565b66ced782
ackermanjayjay <reza23198@gmail.com>
2021-11-20 22:21:32 +0700

    Halaman barang konfig

--- Fix_Proyek/app/src/main/java/com/example/toko_sembako_kelompok5/Halaman_barang.java
+++ Fix_Proyek/app/src/main/java/com/example/toko_sembako_kelompok5/Halaman_barang.java
@@ -10,5 +10,6 @@ public class Halaman_barang extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_barang);
+        getSupportActionBar().setTitle("Halaman barang");
     }
 }
