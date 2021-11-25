4cbdf309927136cab3724afb1f10922b77d1bd98
ackermanjayjay <reza23198@gmail.com>
2021-11-25 17:14:11 +0700

    Hasil operasi total harga barang

--- Fix_Proyek/app/src/main/java/com/example/toko_sembako_kelompok5/Halaman_transaksi.java
+++ Fix_Proyek/app/src/main/java/com/example/toko_sembako_kelompok5/Halaman_transaksi.java
@@ -3,12 +3,22 @@ package com.example.toko_sembako_kelompok5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Halaman_transaksi extends AppCompatActivity {
-
+TextView totalTelur
+        ,totalMinyak;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_transaksi);
+        totalTelur=(TextView)findViewById(R.id.hasil_barang1);
+        totalMinyak=(TextView)findViewById(R.id.hasil_barang2);
+        //        Untuk judul atas
+        getSupportActionBar().setTitle("Halaman Transaksi");
+
+        int dapat=getIntent().getExtras().getInt("HasilTelur");
+//        String hasilTelur=dapat.getString("HasilTelur");
+        totalTelur.setText(Integer.toString(dapat));
     }
 }
